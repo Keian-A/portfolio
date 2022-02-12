e687c1c837158c0a6f80960d2515aa64e3d21768
Keian <KeianAnthony1@gmail.com>
2022-02-11 21:12:56 -0800

    adds linkedin and facebook images/links to footer component

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,12 +1,24 @@
 import '../css/Footer.css';
 import GitHubLogo from '../images/GitHubSmall.png';
+import LinkedInLogo from '../images/LinkedInSmall.png';
+import FacebookIcon from '../images/FacebookSmall.png';
 
 function Footer() {
   return (
     <div className="Footer">
       <div className="GitHubInfo">
-        <a className="GitHubIcon" href="https://github.com/Keian-A" target="_blank" >
-          <img src={GitHubLogo} alt="GitHub" />
+        <a className="GitHubIcon" href="https://github.com/Keian-A" target="_blank" rel="noreferrer" >
+          <img src={GitHubLogo} alt="GitHub Icon" />
+        </a>
+      </div>
+      <div className="LinkedInInfo">
+        <a className="LinkedInIcon" href="https://www.linkedin.com/in/keiananthony/" target="_blank" rel="noreferrer" >
+          <img src={LinkedInLogo} alt="LinkedIn Icon" />
+        </a>
+      </div>
+      <div className="FacebookInfo">
+        <a className="FacebookIcon" href="https://www.facebook.com/Keian.Anthony.1/" target="_blank" rel="noreferrer" >
+          <img src={FacebookIcon} alt="Facebook Icon" />
         </a>
       </div>
     </div>
--- src/components/Home.js
+++ src/components/Home.js
@@ -6,7 +6,7 @@ function Home() {
     <div classname="Home">
       <div className="selfInfo">
         <img className="selfImage" src={myself} alt="myself" />
-        <p className="selfDescription">Hey everyone! My name is Keian Anthony, and I am a full stack software developer. I have previous experience in customer service, where I learned the importance of meeting the needs of the client. I have always been interested in technology ever since I was 8 years old and received my first laptop for my birthday. Growing up with tech got me interested in the hardware side of tech from a young age and I built my 3rd computer by the time I was 18 years old. Recently I have been in love with the software development side of tech. From this interest I have been learning a plethora of languages including JavaScript, Java, C++, Lua, React, and more!</p>
+        <p className="selfDescription">Hey everyone! My name is Keian Anthony, and I am a full stack software developer. I have previous experience in customer service, where I learned the importance of meeting the needs of the client. I have always been interested in technology ever since I was 8 years old and received my first laptop for my birthday. Growing up with tech got me interested in the hardware side of tech from a young age and I built my 3rd computer by the time I was 18 years old. Recently I have been in love with the software development side of tech. I am professionally skilled in a plethora of languages & frameworks including JavaScript, React, Node.js, Java, Lua, and more!</p>
       </div>
     </div>
   );
