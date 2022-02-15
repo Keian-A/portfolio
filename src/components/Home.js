0b25a3380c43065ad6400277c309d4521b952364
Keian Anthony <KeianAnthony1@gmail.com>
2022-02-15 15:45:46 -0800

    adds better layout in Home and Projects for dynamically rendering information later

--- src/components/Home.js
+++ src/components/Home.js
@@ -16,12 +16,16 @@ function Home() {
 
   return (
     <div className="Home">
-      <div className="selfInfo">
+      <div className="selfDescription">
         <img className="selfImage" src={myself} alt="myself" />
-        <div className="selfDescription">
+        <div className="infoDesc">
           <h2>About me</h2>
           <p>Hey all! My name is Keian Anthony, and I am a full stack software developer. I have previous experience in customer service, where I learned the importance of meeting the needs of the client. I have always been interested in technology ever since I was 8 years old and received my first laptop for my birthday. Growing up with tech got me interested in the hardware side of tech from a young age and I built my 3rd computer by the time I was 18 years old. Recently I have been in love with the software development side of tech. I am professionally skilled in a plethora of languages & frameworks including JavaScript, React, Node.js, Java, Lua, and more!</p>
         </div>
+      </div>
+      <div className="pageDescription">
+        <h2>Application Purpose</h2>
+        <p>This page is my current portfolio where you can select the hamburger icon in the top right to navigate to my projects page to view all my projects. There you can search for keywords to find technologies or languages I have made applications with before.</p>
       </div>
     </div>
   );
--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,6 +1,6 @@
 import '../css/Projects.css';
 import AnonMessage from '../images/AnonMessage.png';
-import CAS from '../images/CAS.png';
+// import CAS from '../images/CAS.png';
 import { useEffect } from 'react';
 import darkModeToggle from '../functions/darkModeToggle';
 import lightModeToggle from '../functions/lightModeToggle';
@@ -19,19 +19,20 @@ function Projects() {
     <div className="Projects">
 
       <div className="Project">
-        <h2>AnonyMessage</h2>
-        <p className="ProjectDescription">This application provides the user a real-time, anonymous chatroom</p>
-        <h4>Primary Technologies Used</h4>
-        <p>Socket.io & React</p>
+        <div className="ProjectTop">
+          <h2 className="ProjectDescription" >AnonyMessage</h2>
+          <p className="ProjectDescription" >This application provides the user a real-time, anonymous chatroom</p>
+        </div>
         <div className="InnerInfo">
           <div className="skillsDeveloped">
-            <p id="ptagSkill" className="ptagSkills">Skills developed</p>
+            <p id="ptagSkill" className="ptagSkills">Skills developed:</p>
             <p className="ptagSkills">Communication</p>
             <p className="ptagSkills">Problem solving</p>
-            <p className="ptagSkills">Teamwork</p>
+            <p className="ptagSkills">Documentation</p>
           </div>
           <img className="ProjectImage" src={AnonMessage} alt="project 1 name" />
           <div className="HelpfulLinks">
+            <p id="LinkDesc">Links:</p>
             <a className="ProjectLink" href="https://github.com/BESH-401/anonymessage-frontend" target="_blank" rel="noreferrer" >
               Front-End GitHub Repo
             </a>
@@ -40,27 +41,6 @@ function Projects() {
             </a>
             <a className="ProjectLink" href="https://anony-message.netlify.app/" target="_blank" rel="noreferrer" >
               Deployed Site
-            </a>
-          </div>
-        </div>
-      </div>
-
-      <div className="Project">
-        <h2>Code Against Sanity</h2>
-        <p className="ProjectDescription">This application is our rendition of Cards Against Humanity</p>
-        <h4>Primary Technologies Used</h4>
-        <p>Socket.io & ReadLine</p>
-        <div className="InnerInfo">
-          <div className="skillsDeveloped">
-            <p id="ptagSkill" className="ptagSkills">Skills developed</p>
-            <p className="ptagSkills">Communication</p>
-            <p className="ptagSkills">Problem solving</p>
-            <p className="ptagSkills">Teamwork</p>
-          </div>
-          <img className="ProjectImage" src={CAS} alt="project 1 name" />
-          <div className="HelpfulLinks">
-            <a className="ProjectLink" href="https://github.com/BESH-401" target="_blank" rel="noreferrer" >
-              GitHub Repo
             </a>
           </div>
         </div>
