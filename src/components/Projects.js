e25278c2751ceb69dd7f75b45f827ab1786ac286
Keian Anthony <KeianAnthony1@gmail.com>
2022-02-21 23:46:08 -0800

    preped Projects component to add search functionality, as well as rendering X per -page-

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -16,44 +16,40 @@ function Projects() {
     }
   });
 
-  {
-    return (
-      <div className="Projects">
-
-        {projectData.map((individualProject) => {
-          return (
-
-            <div className="Project">
-              <div className="ProjectTop">
-                <h2 className="ProjectDescription" >{individualProject.projectTitle}</h2>
-                <p className="ProjectDescription" >{individualProject.projectDesc}</p>
+  return (
+    <div className="Projects">
+      {projectData.map((individualProject) => {
+        return (
+          <div className="Project">
+            <div className="ProjectTop">
+              <h2 className="ProjectDescription" >{individualProject.projectTitle}</h2>
+              <p className="ProjectDescription" >{individualProject.projectDesc}</p>
+            </div>
+            <div className="InnerInfo">
+              <div className="skillsDeveloped">
+                {individualProject.skills.map((item) => {
+                  return <p key={item} className="ptagSkills">{item}</p>
+                })}
               </div>
-              <div className="InnerInfo">
-                <div className="skillsDeveloped">
-                  {individualProject.skills.map((item) => {
-                    return <p key={item} className="ptagSkills">{item}</p>
-                  })}
-                </div>
-                <img className="ProjectImage" src={individualProject.image} alt={`project ${individualProject.projectTitle} icon`} />
-                <div className="HelpfulLinks">
-                  <p id="LinkDesc">Links:</p>
-                  {individualProject.links.map((item) => {
-                    let first = item.split('::::::')[0];
-                    let second = item.split('::::::')[1];
-                    return (
-                      <a className="ProjectLink" href={second} target="_blank" rel="noreferrer" >
-                        {first}
-                      </a>
-                    );
-                  })}
-                </div>
+              <img className="ProjectImage" src={individualProject.image} alt={`project ${individualProject.projectTitle} icon`} />
+              <div className="HelpfulLinks">
+                <p id="LinkDesc">Links:</p>
+                {individualProject.links.map((item) => {
+                  let first = item.split('::::::')[0];
+                  let second = item.split('::::::')[1];
+                  return (
+                    <a className="ProjectLink" href={second} target="_blank" rel="noreferrer" >
+                      {first}
+                    </a>
+                  );
+                })}
               </div>
             </div>
-          );
-        })};
-      </div>
-    );
-  }
+          </div>
+        );
+      })}
+    </div>
+  );
 }
 
 export default Projects;
--- src/data/projectData.js
+++ src/data/projectData.js
@@ -1,5 +1,6 @@
 const testData = [
     {
+        techUsed: ['socket.io', 'react', 'javascript', 'nodejs', 'node.js'],
         projectTitle: 'AnonyMessage',
         projectDesc: 'This application provides the user a real-time, anonymous chatroom.',
         skills: ['Communication', 'Problem solving', 'Documentation'],
@@ -7,11 +8,20 @@ const testData = [
         image: require('../images/AnonMessage.png')
     },
     {
-        projectTitle: 'AnonyMessage',
-        projectDesc: 'This application provides the user a real-time, anonymous chatroom.',
-        skills: ['Communication', 'Problem solving', 'Documentation'],
-        links: ['Front-end repository::::::https://github.com/BESH-401/anonymessage-frontend', 'Back-end Repository::::::https://github.com/BESH-401/anonymessage-backend', 'Deployed site::::::https://anony-message.netlify.app/'],
-        image: require('../images/AnonMessage.png')
+        techUsed: ['socket.io', 'javascript', 'nodejs', 'node.js'],
+        projectTitle: 'Code Against Sanity',
+        projectDesc: 'This application is our adaptation of Cards Against Humanity, played online with user input through a terminal.',
+        skills: ['Team work', 'Communication', 'Problem solving'],
+        links: ['Back-end repository::::::https://github.com/CodeAgainstSanity/CAS-backend'],
+        image: require('../images/CAS.png')
+    },
+    {
+        techUsed: ['socket.io', 'javascript', 'nodejs', 'node.js'],
+        projectTitle: 'Socki-Talki',
+        projectDesc: 'This application provides the user the ability to create personal chat rooms, and talk to other people in the same room as them.',
+        skills: ['Collaboration', 'Creativity', 'Organization'],
+        links: ['Back-end repository::::::https://github.com/socki-talki/socki-talki'],
+        image: require('../images/sockiTalki.png')
     }
 ];
 
